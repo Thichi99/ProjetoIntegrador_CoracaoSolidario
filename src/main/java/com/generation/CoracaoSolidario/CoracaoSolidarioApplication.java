3a2c21574baa3f245b2ce72d07adfbdedb300474
Thiago dos Passos Lima <166858263+Thichi99@users.noreply.github.com>
2024-07-05 14:15:22 -0300

    Criação do Projeto e da Classe Model Categoria

---
+++ src/main/java/com/generation/CoracaoSolidario/CoracaoSolidarioApplication.java
@@ -0,0 +1,13 @@
+package com.generation.CoracaoSolidario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoracaoSolidarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoracaoSolidarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/CoracaoSolidario/CoracaoSolidarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.CoracaoSolidario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoracaoSolidarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
