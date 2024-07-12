1438f0ee332db57cbd4db72bc399d16aea0e240e
Camila Vildoso <camiliun123@gmail.com>
2024-07-12 12:21:51 -0300

    Atualizar tabela

--- src/main/java/com/generation/CoracaoSolidario/model/Doador.java
+++ src/main/java/com/generation/CoracaoSolidario/model/Doador.java
@@ -1,10 +1,14 @@
 package com.generation.CoracaoSolidario.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "tb_doadores")
 public class Doador {
 
 	@Id
