226cd86520cc41b3e8efef931eebb17c3137349d
Wellington Albino <well.renato@hotmail.com>
2024-07-23 11:00:36 -0300

    task_07

--- src/main/java/com/generation/CoracaoSolidario/model/Usuario.java
+++ src/main/java/com/generation/CoracaoSolidario/model/Usuario.java
@@ -1,6 +1,7 @@
 package com.generation.CoracaoSolidario.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -20,6 +21,7 @@ public class Usuario {
     @NotNull(message = "O atributo Nome é obrigatório!")
     private String nome;
 
+    @Schema(example = "email@email.com.br")
     @NotNull(message = "O atributo Usuario é obrigatório!")
     @Email(message = "O Atributo Usuário deve ser um email válido!")
     private String usuario;
